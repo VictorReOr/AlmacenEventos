1c707ed0ec10039c2ae305e463b10237bd2978f0
Víctor R <victor.reina@juntadeandalucia.es>
2026-01-27 14:28:38 +0100

    feat(auth): set google client id

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import { AuthProvider } from './context/AuthContext';
 
 // TODO: Replace with actual Google Client ID from Cloud Console
-const GOOGLE_CLIENT_ID = "YOUR_GOOGLE_CLIENT_ID_HERE.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "856058698301-4rk59qb6j7d75r72ecntmgiv3tu126o6.apps.googleusercontent.com";
 
 console.log('Main script executing');
 createRoot(document.getElementById('root')!).render(
